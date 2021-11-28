330821b773ba41874c3d407fd9f18cd8e1ab3607
Noam Tamim <noam.tamim@kaltura.com>
2021-11-28 17:42:14 +0200

    Fix

---
+++ setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='ua-parser',
+    version='0.15.0',
+    packages=['ua_parser'],
+    url='https://github.com/kaltura/uap-python',
+    license='Apache2',
+    zip_safe=False
+)
